0efd12f267ad794a453286f3c997aff9e0a6d5b8
Huỳnh Quang Thái <thai.huynh@pap-tech.com>
2021-07-22 17:12:51 +0700

    Create test project

---
+++ index.js
@@ -0,0 +1,10 @@
+function robotSays(message = '') {
+    let robot = '[o.o]'
+
+    if (message === '')
+        return `${robot} Hello!!!`
+
+    return `${robot} ${message}`
+}
+
+module.exports.robotSays = robotSays
